a8ed6c2c9089259e01c02a2a90dbd5ee3ad201f9
fil7 <filonova7@yandex.ru>
2016-11-16 14:20:11 +0700

    - added new data for table

--- src/resources/database/fillTasks.sql
+++ src/resources/database/fillTasks.sql
@@ -1,3 +1,8 @@
-INSERT INTO `test`.`tasks` (ID, DESCRIPTION, STATE) VALUES ('1', 'buy milk', 0);
-INSERT INTO `test`.`tasks` (ID, DESCRIPTION, STATE) VALUES ('2', 'read the book', 0);
-INSERT INTO `test`.`tasks` (ID, DESCRIPTION, STATE) VALUES ('3', 'help a friend', 0);
+INSERT INTO `test`.`tasks` (DESCRIPTION, STATE, PRIORITY) VALUES ('buy milk', 0, 2);
+INSERT INTO `test`.`tasks` (DESCRIPTION, STATE, PRIORITY) VALUES ('read the book', 0, 1);
+INSERT INTO `test`.`tasks` (DESCRIPTION, STATE, PRIORITY) VALUES ('help a friend', 0, 1);
+INSERT INTO `test`.`tasks` (DESCRIPTION, STATE, PRIORITY) VALUES ('respond email', 0, 0);
+INSERT INTO `test`.`tasks` (DESCRIPTION, STATE, PRIORITY) VALUES ('find new info', 1, 0);
+INSERT INTO `test`.`tasks` (DESCRIPTION, STATE, PRIORITY) VALUES ('learn java', 0, 2);
+INSERT INTO `test`.`tasks` (DESCRIPTION, STATE, PRIORITY) VALUES ('english: lingualeo', 1, 2);
+INSERT INTO `test`.`tasks` (DESCRIPTION, STATE, PRIORITY) VALUES ('cleaning', 1, 0);
